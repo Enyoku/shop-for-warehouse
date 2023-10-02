bad763e84692e5a8f194c3a7971e3f1353488f5c
enyoku <mondex.2003@gmail.com>
2023-10-02 15:16:33 +0300

    Some fixes in new_order view

--- account/serializers.py
+++ account/serializers.py
@@ -19,3 +19,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email')
+
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "first_name", "last_name", "email")
--- order/serializers.py
+++ order/serializers.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from order.models import Order, OrderList
+from account.serializers import ClientSerializer
 
 
 class OrderListSerializer(serializers.ModelSerializer):
@@ -11,6 +13,7 @@ class OrderListSerializer(serializers.ModelSerializer):
 
 class OrderCreateSerializer(serializers.ModelSerializer):
     orderList = serializers.SerializerMethodField(method_name='get_order_list', read_only=True)
+    user = serializers.SerializerMethodField(method_name="get_user_credentials", read_only=True)
     class Meta:
         model = Order
         fields = "__all__"
@@ -19,13 +22,18 @@ class OrderCreateSerializer(serializers.ModelSerializer):
         order_list = obj.orderlistitems.all()
         serializer = OrderListSerializer(order_list, many=True)
         return serializer.data
+    
+    def get_user_credentials(self, obj):
+        user = obj.user
+        serializer = ClientSerializer(user, many=False)
+        return serializer.data
 
 
 class OrderReadSerializer(serializers.ModelSerializer):
     first_name = serializers.SerializerMethodField()
     last_name = serializers.SerializerMethodField
     orderItems = serializers.SerializerMethodField(method_name='get_order_items', read_only=True)
-    
+
     class Meta:
         model = Order
         fields = ("first_name", "last_name", "total_price", "order_status", "payment_status", "delivery_address")
